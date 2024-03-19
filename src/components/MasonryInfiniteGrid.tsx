df5d16e129c796640829ac7c80da1f6784aad24f
BY-juun <neostgeart@gmail.com>
2024-03-19 16:20:29 +0900

    feat: GridWrapper에 ref 주입을 위해 타입 수정

--- src/components/MasonryInfiniteGrid.tsx
+++ src/components/MasonryInfiniteGrid.tsx
@@ -1,16 +1,24 @@
-import React, { PropsWithChildren } from "react";
+import React, { ElementType, PropsWithChildren, useRef } from "react";
 
-interface Props {
-  tagName?: keyof JSX.IntrinsicElements;
+type Props<T extends keyof JSX.IntrinsicElements> = {
+  tagName?: T;
   className?: string;
-}
+} & JSX.IntrinsicElements[T];
 
-const MasonryInfiniteGrid = ({
+const MasonryInfiniteGrid = <T extends keyof JSX.IntrinsicElements>({
   children,
-  tagName: Wrapper = "div",
+  tagName: Wrapper = "div" as T,
   ...rest
-}: PropsWithChildren<Props>) => {
-  return <Wrapper {...rest}>{children}</Wrapper>;
+}: PropsWithChildren<Props<T>>) => {
+  const gridWrapperRef = useRef<any>(null);
+
+  const GridWrapperComponent = Wrapper as ElementType;
+
+  return (
+    <GridWrapperComponent ref={gridWrapperRef} {...rest}>
+      {children}
+    </GridWrapperComponent>
+  );
 };
 
 export default MasonryInfiniteGrid;
