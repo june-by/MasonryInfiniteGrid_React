2103292e9399177de342e05390d1e10b49c06d9a
BY-juun <neostgeart@gmail.com>
2024-03-19 13:30:12 +0900

    feat: className props 추가

--- src/components/MasonryInfiniteGrid.tsx
+++ src/components/MasonryInfiniteGrid.tsx
@@ -2,13 +2,15 @@ import React, { PropsWithChildren } from "react";
 
 interface Props {
   tagName?: keyof JSX.IntrinsicElements;
+  className?: string;
 }
 
 const MasonryInfiniteGrid = ({
   children,
   tagName: Wrapper = "div",
+  ...rest
 }: PropsWithChildren<Props>) => {
-  return <Wrapper>{children}</Wrapper>;
+  return <Wrapper {...rest}>{children}</Wrapper>;
 };
 
 export default MasonryInfiniteGrid;
