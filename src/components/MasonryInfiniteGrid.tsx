7bee0e810e52c19a3c79a6e91d525645d568b910
BY-juun <neostgeart@gmail.com>
2024-03-20 13:48:00 +0900

    feat: GridItem 절대값 위치 계산 로직 추가

--- src/components/MasonryInfiniteGrid.tsx
+++ src/components/MasonryInfiniteGrid.tsx
@@ -1,16 +1,70 @@
-import React, { ElementType, PropsWithChildren, useRef } from "react";
+import React, {
+  ElementType,
+  PropsWithChildren,
+  useLayoutEffect,
+  useRef,
+} from "react";
 
 type Props<T extends keyof JSX.IntrinsicElements> = {
   tagName?: T;
   className?: string;
 } & JSX.IntrinsicElements[T];
 
+const getNumOfGridColumn = (gridItemWidth: number) => {
+  let numOfCol = 0;
+  let accWidth = 0;
+
+  while (accWidth < window.innerWidth) {
+    accWidth += gridItemWidth;
+    numOfCol++;
+  }
+
+  return numOfCol || 1;
+};
+
 const MasonryInfiniteGrid = <T extends keyof JSX.IntrinsicElements>({
   children,
   tagName: Wrapper = "div" as T,
   ...rest
 }: PropsWithChildren<Props<T>>) => {
-  const gridWrapperRef = useRef<any>(null);
+  const gridWrapperRef = useRef<HTMLElement | null>(null);
+
+  useLayoutEffect(() => {
+    const gridWrapperElement = gridWrapperRef.current;
+    if (!gridWrapperElement) {
+      return;
+    }
+
+    gridWrapperElement.style.visibility = "hidden";
+
+    const gridItemElements = Array.from(gridWrapperElement.children);
+
+    // 모든 Child의 Width가 동일하다고 가정합니다.
+    const childWidth = gridItemElements[0].getBoundingClientRect().width;
+
+    // 전체 Grid Column의 개수
+    const numOfCol = getNumOfGridColumn(childWidth);
+
+    // 긱 Column 별 Height 누적 값
+    const accHeightPerColumn = Array.from({ length: numOfCol }, () => 0);
+
+    // 위치 계산
+    gridItemElements.forEach((gridItemElement) => {
+      const currentColIdx = accHeightPerColumn.indexOf(
+        Math.min(...accHeightPerColumn)
+      );
+
+      gridItemElement.setAttribute(
+        "style",
+        `position : absolute; left : ${childWidth * currentColIdx}px; top : ${
+          accHeightPerColumn[currentColIdx]
+        }px`
+      );
+      accHeightPerColumn[currentColIdx] += gridItemElement.clientHeight;
+    });
+
+    gridWrapperElement.style.visibility = "";
+  }, []);
 
   const GridWrapperComponent = Wrapper as ElementType;
 
