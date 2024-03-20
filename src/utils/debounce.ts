1205ba71808729a818887b3f09caa090aa66b8c3
BY-juun <neostgeart@gmail.com>
2024-03-20 14:48:57 +0900

    feat: resize 이벤트 핸들러에 대한 debounce를 위해 유틸함수 추가

---
+++ src/utils/debounce.ts
@@ -0,0 +1,15 @@
+const debounce = <F extends (...args: any) => any>(
+  func: F,
+  waitFor: number
+) => {
+  let timeout: NodeJS.Timeout | number = 0;
+
+  const debounced = (...args: any) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func(...args), waitFor);
+  };
+
+  return debounced as (...args: Parameters<F>) => ReturnType<F>;
+};
+
+export default debounce;
