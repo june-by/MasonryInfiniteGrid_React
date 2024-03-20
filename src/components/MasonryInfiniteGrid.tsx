82d7608c7619722c620f7fdbc1f956a2d98c56a2
BY-juun <neostgeart@gmail.com>
2024-03-20 13:55:01 +0900

    refactor: GridItem 위치 계산 함수를 다른 훅에서 재활용하기 위해 컴포넌트 안으로 이동

--- src/components/MasonryInfiniteGrid.tsx
+++ src/components/MasonryInfiniteGrid.tsx
@@ -1,6 +1,7 @@
 import React, {
   ElementType,
   PropsWithChildren,
+  useCallback,
   useLayoutEffect,
   useRef,
 } from "react";
@@ -22,40 +23,46 @@ const getNumOfGridColumn = (gridItemWidth: number) => {
   return numOfCol || 1;
 };
 
-const calculateGridItemsPos = (gridWrapperElement: HTMLElement) => {
-  const gridItemElements = Array.from(gridWrapperElement.children);
-
-  // 모든 Child의 Width가 동일하다고 가정합니다.
-  const childWidth = gridItemElements[0].getBoundingClientRect().width;
-
-  // 전체 Grid Column의 개수
-  const numOfCol = getNumOfGridColumn(childWidth);
-
-  // 긱 Column 별 Height 누적 값
-  const accHeightPerColumn = Array.from({ length: numOfCol }, () => 0);
-
-  // 위치 계산
-  gridItemElements.forEach((gridItemElement) => {
-    const currentColIdx = accHeightPerColumn.indexOf(
-      Math.min(...accHeightPerColumn)
-    );
-
-    gridItemElement.setAttribute(
-      "style",
-      `position : absolute; left : ${childWidth * currentColIdx}px; top : ${
-        accHeightPerColumn[currentColIdx]
-      }px`
-    );
-    accHeightPerColumn[currentColIdx] += gridItemElement.clientHeight;
-  });
-};
-
 const MasonryInfiniteGrid = <T extends keyof JSX.IntrinsicElements>({
   children,
   tagName: Wrapper = "div" as T,
   ...rest
 }: PropsWithChildren<Props<T>>) => {
   const gridWrapperRef = useRef<HTMLElement | null>(null);
+
+  const calculateGridItemsPos = useCallback(() => {
+    if (!gridWrapperRef.current) {
+      return;
+    }
+
+    const gridWrapperElement = gridWrapperRef.current;
+
+    const gridItemElements = Array.from(gridWrapperElement.children);
+
+    // 모든 Child의 Width가 동일하다고 가정합니다.
+    const childWidth = gridItemElements[0].getBoundingClientRect().width;
+
+    // 전체 Grid Column의 개수
+    const numOfCol = getNumOfGridColumn(childWidth);
+
+    // 긱 Column 별 Height 누적 값
+    const accHeightPerColumn = Array.from({ length: numOfCol }, () => 0);
+
+    // 위치 계산
+    gridItemElements.forEach((gridItemElement) => {
+      const currentColIdx = accHeightPerColumn.indexOf(
+        Math.min(...accHeightPerColumn)
+      );
+
+      gridItemElement.setAttribute(
+        "style",
+        `position : absolute; left : ${childWidth * currentColIdx}px; top : ${
+          accHeightPerColumn[currentColIdx]
+        }px`
+      );
+      accHeightPerColumn[currentColIdx] += gridItemElement.clientHeight;
+    });
+  }, []);
 
   useLayoutEffect(() => {
     const gridWrapperElement = gridWrapperRef.current;
@@ -65,10 +72,10 @@ const MasonryInfiniteGrid = <T extends keyof JSX.IntrinsicElements>({
 
     gridWrapperElement.style.visibility = "hidden";
 
-    calculateGridItemsPos(gridWrapperElement);
+    calculateGridItemsPos();
 
     gridWrapperElement.style.visibility = "";
-  }, []);
+  }, [calculateGridItemsPos]);
 
   const GridWrapperComponent = Wrapper as ElementType;
 
