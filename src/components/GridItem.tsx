321964cf8f3318b460ddef8de0996d56ff11ceb9
BY-juun <neostgeart@gmail.com>
2024-03-20 16:40:12 +0900

    feat: GridItem이 추가되는 상황을 눈으로 확인하기 위해 Group 넘버 추가

--- src/components/GridItem.tsx
+++ src/components/GridItem.tsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useMemo } from "react";
+
+const COLORS = ["white", "blue", "green", "red", "yellow"];
 
 interface Props {
   index: number;
 }
 
 const GridItem = ({ index }: Props) => {
-  const randomIdx = Math.floor(Math.random() * 6 + 1);
+  const randomIdx = useMemo(() => Math.floor(Math.random() * 6 + 1), []);
 
   return (
     <li className={`grid_item height_${randomIdx}`}>
-      <div className="grid_item_content">GirdItem{index}</div>
+      <div className={`grid_item_content`}>
+        <div>
+          <p>GirdItem{index}</p>
+          <p>Group : {Math.floor(index / 20)}</p>
+        </div>
+      </div>
     </li>
   );
 };
