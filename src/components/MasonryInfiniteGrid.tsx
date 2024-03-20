cdebe1c1101df07f7b2a9950a9bceb2b804eb93e
BY-juun <neostgeart@gmail.com>
2024-03-20 13:51:53 +0900

    refactor: GridItem 위치 계산 로직 함수 분리

--- src/components/MasonryInfiniteGrid.tsx
+++ src/components/MasonryInfiniteGrid.tsx
@@ -22,6 +22,34 @@ const getNumOfGridColumn = (gridItemWidth: number) => {
   return numOfCol || 1;
 };
 
+const calculateGridItemsPos = (gridWrapperElement: HTMLElement) => {
+  const gridItemElements = Array.from(gridWrapperElement.children);
+
+  // 모든 Child의 Width가 동일하다고 가정합니다.
+  const childWidth = gridItemElements[0].getBoundingClientRect().width;
+
+  // 전체 Grid Column의 개수
+  const numOfCol = getNumOfGridColumn(childWidth);
+
+  // 긱 Column 별 Height 누적 값
+  const accHeightPerColumn = Array.from({ length: numOfCol }, () => 0);
+
+  // 위치 계산
+  gridItemElements.forEach((gridItemElement) => {
+    const currentColIdx = accHeightPerColumn.indexOf(
+      Math.min(...accHeightPerColumn)
+    );
+
+    gridItemElement.setAttribute(
+      "style",
+      `position : absolute; left : ${childWidth * currentColIdx}px; top : ${
+        accHeightPerColumn[currentColIdx]
+      }px`
+    );
+    accHeightPerColumn[currentColIdx] += gridItemElement.clientHeight;
+  });
+};
+
 const MasonryInfiniteGrid = <T extends keyof JSX.IntrinsicElements>({
   children,
   tagName: Wrapper = "div" as T,
@@ -37,31 +65,7 @@ const MasonryInfiniteGrid = <T extends keyof JSX.IntrinsicElements>({
 
     gridWrapperElement.style.visibility = "hidden";
 
-    const gridItemElements = Array.from(gridWrapperElement.children);
-
-    // 모든 Child의 Width가 동일하다고 가정합니다.
-    const childWidth = gridItemElements[0].getBoundingClientRect().width;
-
-    // 전체 Grid Column의 개수
-    const numOfCol = getNumOfGridColumn(childWidth);
-
-    // 긱 Column 별 Height 누적 값
-    const accHeightPerColumn = Array.from({ length: numOfCol }, () => 0);
-
-    // 위치 계산
-    gridItemElements.forEach((gridItemElement) => {
-      const currentColIdx = accHeightPerColumn.indexOf(
-        Math.min(...accHeightPerColumn)
-      );
-
-      gridItemElement.setAttribute(
-        "style",
-        `position : absolute; left : ${childWidth * currentColIdx}px; top : ${
-          accHeightPerColumn[currentColIdx]
-        }px`
-      );
-      accHeightPerColumn[currentColIdx] += gridItemElement.clientHeight;
-    });
+    calculateGridItemsPos(gridWrapperElement);
 
     gridWrapperElement.style.visibility = "";
   }, []);
