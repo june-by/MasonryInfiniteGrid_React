a41f06f4be65c2e117476cd86cd8915b3daf3433
BY-juun <neostgeart@gmail.com>
2024-03-21 14:37:53 +0900

    feat: Skeleton 추가

---
+++ src/components/Skeleton.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Skeleton = () => {
+  return (
+    <div className="skeleton_wrap">
+      <div className="skeleton_content" />
+    </div>
+  );
+};
+
+export default Skeleton;
