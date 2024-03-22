e51c0472b005ac0ebd3ce0a80ee6a7ef427ba7ae
BY-juun <neostgeart@gmail.com>
2024-03-22 11:43:49 +0900

    feat: ref, useImpreativeHandle을 통해 외부에 renderItems 메서드 제공

--- src/components/MasonryInfiniteGrid.tsx
+++ src/components/MasonryInfiniteGrid.tsx
@@ -2,6 +2,7 @@ import React, {
   ElementType,
   PropsWithChildren,
   useCallback,
+  useImperativeHandle,
   useLayoutEffect,
   useRef,
   useState,
@@ -30,97 +31,117 @@ const getNumOfGridColumn = (gridItemWidth: number) => {
   return numOfCol || 1;
 };
 
-const MasonryInfiniteGrid = <T extends keyof JSX.IntrinsicElements>({
-  children,
-  tagName: Wrapper = "div" as T,
-  resizeDebounce = 1000,
-  hasMore = false,
-  fetchNext = () => Promise.resolve(),
-  skeleton,
-  ...rest
-}: PropsWithChildren<Props<T>>) => {
-  const isInitialGridRender = useRef(true);
-  const [isFetchingNextLoading, setIsFetchingNextLoading] = useState(false);
-  const gridWrapperRef = useRef<HTMLElement | null>(null);
+const MasonryInfiniteGrid = React.forwardRef(
+  <T extends keyof JSX.IntrinsicElements>(
+    {
+      children,
+      tagName: Wrapper = "div" as T,
+      resizeDebounce = 1000,
+      hasMore = false,
+      fetchNext = () => Promise.resolve(),
+      skeleton,
+      ...rest
+    }: PropsWithChildren<Props<T>>,
+    ref?: React.Ref<{
+      renderItems: () => void;
+    }>
+  ) => {
+    const isInitialGridRender = useRef(true);
+    const [isFetchingNextLoading, setIsFetchingNextLoading] = useState(false);
+    const gridWrapperRef = useRef<HTMLElement | null>(null);
 
-  const calculateGridItemsPos = useCallback(() => {
-    if (!gridWrapperRef.current) {
-      return;
-    }
+    const calculateGridItemsPos = useCallback(() => {
+      if (!gridWrapperRef.current) {
+        return;
+      }
 
-    const gridWrapperElement = gridWrapperRef.current;
+      const gridWrapperElement = gridWrapperRef.current;
 
-    const gridItemElements = Array.from(gridWrapperElement.children);
+      const gridItemElements = Array.from(gridWrapperElement.children);
 
-    // 모든 Child의 Width가 동일하다고 가정합니다.
-    const childWidth = gridItemElements[0].getBoundingClientRect().width;
+      // 모든 Child의 Width가 동일하다고 가정합니다.
+      const childWidth = gridItemElements[0].getBoundingClientRect().width;
 
-    // 전체 Grid Column의 개수
-    const numOfCol = getNumOfGridColumn(childWidth);
+      // 전체 Grid Column의 개수
+      const numOfCol = getNumOfGridColumn(childWidth);
 
-    // 긱 Column 별 Height 누적 값
-    const accHeightPerColumn = Array.from({ length: numOfCol }, () => 0);
+      // 긱 Column 별 Height 누적 값
+      const accHeightPerColumn = Array.from({ length: numOfCol }, () => 0);
 
-    // 위치 계산
-    gridItemElements.forEach((gridItemElement, idx) => {
-      const currentColIdx = accHeightPerColumn.indexOf(
-        Math.min(...accHeightPerColumn)
-      );
+      // 위치 계산
+      gridItemElements.forEach((gridItemElement, idx) => {
+        const currentColIdx = accHeightPerColumn.indexOf(
+          Math.min(...accHeightPerColumn)
+        );
 
-      gridItemElement.setAttribute(
-        "style",
-        `position : absolute; left : ${childWidth * currentColIdx}px; top : ${
-          accHeightPerColumn[currentColIdx]
-        }px`
-      );
-      accHeightPerColumn[currentColIdx] += gridItemElement.clientHeight;
+        gridItemElement.setAttribute(
+          "style",
+          `position : absolute; left : ${childWidth * currentColIdx}px; top : ${
+            accHeightPerColumn[currentColIdx]
+          }px`
+        );
+        accHeightPerColumn[currentColIdx] += gridItemElement.clientHeight;
+      });
+
+      gridWrapperElement.style.height = `${Math.max(...accHeightPerColumn)}px`;
+    }, []);
+
+    const renderItems = useCallback(() => {
+      if (isFetchingNextLoading && !skeleton) {
+        return;
+      }
+
+      const gridWrapperElement = gridWrapperRef.current;
+      if (!gridWrapperElement) {
+        return;
+      }
+
+      if (isInitialGridRender.current)
+        gridWrapperElement.style.visibility = "hidden";
+
+      calculateGridItemsPos();
+
+      if (isInitialGridRender.current) {
+        gridWrapperElement.style.visibility = "";
+        isInitialGridRender.current = false;
+      }
+    }, [calculateGridItemsPos, isFetchingNextLoading, skeleton]);
+
+    useLayoutEffect(() => {
+      renderItems();
+    }, [renderItems]);
+
+    useResize(calculateGridItemsPos, resizeDebounce);
+
+    useImperativeHandle(ref, () => {
+      return {
+        renderItems: () => {
+          isInitialGridRender.current = true;
+          renderItems();
+        },
+      };
     });
 
-    gridWrapperElement.style.height = `${Math.max(...accHeightPerColumn)}px`;
-  }, []);
+    const GridWrapperComponent = Wrapper as ElementType;
 
-  useLayoutEffect(() => {
-    if (isFetchingNextLoading && !skeleton) {
-      return;
-    }
-
-    const gridWrapperElement = gridWrapperRef.current;
-    if (!gridWrapperElement) {
-      return;
-    }
-
-    if (isInitialGridRender.current)
-      gridWrapperElement.style.visibility = "hidden";
-
-    calculateGridItemsPos();
-
-    if (isInitialGridRender.current) {
-      gridWrapperElement.style.visibility = "";
-      isInitialGridRender.current = false;
-    }
-  }, [calculateGridItemsPos, isFetchingNextLoading, skeleton]);
-
-  useResize(calculateGridItemsPos, resizeDebounce);
-
-  const GridWrapperComponent = Wrapper as ElementType;
-
-  return (
-    <InfiniteScroll
-      fetchNext={() => {
-        if (isFetchingNextLoading) {
-          return;
-        }
-        setIsFetchingNextLoading(true);
-        fetchNext().finally(() => setIsFetchingNextLoading(false));
-      }}
-      hasMore={hasMore}
-    >
-      <GridWrapperComponent ref={gridWrapperRef} {...rest}>
-        {children}
-        {isFetchingNextLoading && skeleton}
-      </GridWrapperComponent>
-    </InfiniteScroll>
-  );
-};
+    return (
+      <InfiniteScroll
+        fetchNext={() => {
+          if (isFetchingNextLoading) {
+            return;
+          }
+          setIsFetchingNextLoading(true);
+          fetchNext().finally(() => setIsFetchingNextLoading(false));
+        }}
+        hasMore={hasMore}
+      >
+        <GridWrapperComponent ref={gridWrapperRef} {...rest}>
+          {children}
+          {isFetchingNextLoading && skeleton}
+        </GridWrapperComponent>
+      </InfiniteScroll>
+    );
+  }
+);
 
 export default MasonryInfiniteGrid;
